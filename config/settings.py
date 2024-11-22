7dfd57a6af27bdcf4c3f81cfbe2f433dd33cd022
Alex Little <consult@alexlittle.net>
2024-11-23 00:05:35 +0200

    update for validation

--- config/settings.py
+++ config/settings.py
@@ -79,7 +79,7 @@ WSGI_APPLICATION = 'config.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': 'db.sqlite3',
+        'NAME': '',
     }
 }
 
